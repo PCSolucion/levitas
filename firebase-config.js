17bcbf1a75dd413911bcba21c75eaef4255719b8
Liukin <contacto@pc-solucion.es>
2026-03-05 04:29:29 +0100

    Clean up project files and transition to vanilla JS

--- firebase-config.js
+++ firebase-config.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyBRFm-MiIoj5YE78S9cGCwqMzwFI8rw-E8",
+  authDomain: "fastingapp-193de.firebaseapp.com",
+  projectId: "fastingapp-193de",
+  storageBucket: "fastingapp-193de.firebasestorage.app",
+  messagingSenderId: "440317081640",
+  appId: "1:440317081640:web:36cfff85d20432795d1d1d"
 };
 
 // Inicializar Firebase
